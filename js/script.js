0ff10a10170d0d64d13443dbb921a0b670775d76
Pedro Henrique <petertec369@gmail.com>
2025-10-01 15:52:09 -0300

    parametros-url-js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ATIVAR LINKS MENU
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ATIVAR ITEMS DO ORCAMENTO
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
