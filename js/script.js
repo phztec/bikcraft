6d598fefd526f84f89505471e258d50efedaddae
Pedro Henrique <petertec369@gmail.com>
2025-10-01 16:34:10 -0300

    galeria-imagens-js

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function ativarPergunta(event) {
   const resposta = document.getElementById(controls);
 
   resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa")
+  const ativa = resposta.classList.contains("ativa");
 
   pergunta.setAttribute("aria-expanded", ativa);
 }
@@ -43,3 +43,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// GALERIA DE BICICLETAS
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
